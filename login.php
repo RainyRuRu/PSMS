26bd8c41569971819b38aad525c5d0b03aaba222
snoopy2199 <snoopy2199@gmail.com>
2015-12-08 23:33:19 +0800

    modified login.php

--- login.php
+++ login.php
@@ -1,9 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Hello World!</title>
+	<title>人事薪資管理系統</title>
+	<meta charset="utf8">
+	<!-- 最新編譯和最佳化的 CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+	<!-- 選擇性佈景主題 -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+
+	<!-- 最新編譯和最佳化的 JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 <head>
 <body>
-	Hello World!
+	<div class="container">
+		<center style="margin-top:100px;">
+			<h1>人事薪資管理系統<h1>
+				<form>
+					<div class="form-horizontal">
+						<div class="form-group">
+							<div class="col-sm-offset-4 col-sm-4">
+								<input type="text" class="form-control" name="name" placeholder="帳號">
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-4 col-sm-4">
+								<input type="text" class="form-control" name="password" placeholder="密碼">
+							</div>
+						</div>
+						<button class="btn btn-default" type="submit" >登入</button>
+					</div>
+				</form>
+		</center>
+	</div>
 </body>
 </html>

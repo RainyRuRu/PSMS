33aabc6d4de8f3930eb3cda47163067368c3b732
snoopy2199 <snoopy2199@gmail.com>
2015-12-15 01:23:48 +0800

    so SAD!!!!!!!!!

--- api/getDownloadDetail.php
+++ api/getDownloadDetail.php
@@ -2,12 +2,13 @@
 
 $input_id = $_POST[id];
 $input_year = $_POST[year];
+$input_month = $_POST[month];
 
-$json = array('data' => countFile($input_id, $input_year));
+$json = array('data' => getDownloadDetail($input_id, $input_year, $input_month));
 $json_en = json_encode($json);
 echo $json_en;
 
-function countFile($id, $year) {
+function getDownloadDetail($id, $year, $month) {
 	$db_host = "127.0.0.1";
 	$db_user = "root";
 	$db_pass = "04241104";
@@ -17,12 +18,12 @@ function countFile($id, $year) {
 	$db_go = new PDO($db_connect, $db_user, $db_pass);
 	$db_go -> exec("SET NAMES UTF8");
 
-	$sql = $db_go -> prepare("SELECT month, COUNT(path) AS count
+	$sql = $db_go -> prepare("SELECT path, upload
 							  FROM payroll 
-							  WHERE user_id = :id AND year = :year 
-							  GROUP BY month");
+							  WHERE user_id = :id AND year = :year AND month = :month");
 	$sql -> bindParam(":id", $id, PDO::PARAM_STR, 10);
 	$sql -> bindParam(":year", $year, PDO::PARAM_INT, 4);
+	$sql -> bindParam(":month", $month, PDO::PARAM_INT, 2);
 	$sql -> execute();
 
 	$result = $sql -> fetchAll();
--- js/employee.js
+++ js/employee.js
@@ -1,8 +1,16 @@
+var nowYear;
+
 function logout() {
     window.location = "api/doLogout.php";
 }
 
 function goYear(id, year) {
+	if(year == nowYear) {
+		return;
+	}
+
+	resetDownload();
+
 	$.ajax({
 		url: "api/getYearPayroll.php",
 		data: {
@@ -12,15 +20,17 @@ function goYear(id, year) {
 		type: "POST",
 		dataType: "json",
 		success: function(msg){
-			for (i = 0; i < msg.data.length; i++) {
+			for (var i = 0; i < msg.data.length; i++) {
 				var count = msg.data[i].count == "1" ? "" : msg.data[i].count;
-				$("#month" + msg.data[i].month).append($('<div class="row titleRow"><button type="button" class="btn btn-info col-md-offset-4">Download <span class="badge">' + count + '</span></button></div>'));
+				$("#month" + msg.data[i].month).append($('<div id="download'+ msg.data[i].month +'" class="row titleRow"><button type="button" class="btn btn-info col-md-offset-4" onclick=clickDownload("' + id + '","' + msg.data[i].month + '")>Download <span class="badge">' + count + '</span></button></div>'));
 			}
 		},
 		error: function(xhr) {
-      		alert("Ajax request 發生錯誤");
-    	}
+			alert("Ajax request 發生錯誤");
+		}
 	});
+
+	nowYear = getYear();
 }
 
 function getYear() {
@@ -44,5 +54,30 @@ function onLoad(id) {
 }
 
 function resetDownload() {
-	
+	for (var i = 1; i <= 12; i++) {
+		$("#download"+i).remove();
+	};
+}
+
+function clickDownload(id, month) {
+	alert("!!!!");
+	$.ajax({
+		url: "api/getDownloadDetail.php",
+		data: {
+			id: id,
+			year: nowYear,
+			month: month
+
+		},
+		type: "POST",
+		dataType: "json",
+		success: function(msg){
+			if(msg.data.length == 1) {
+				window.open(msg.data[0].path);
+			}
+		},
+		error: function(xhr) {
+			alert("Ajax request 發生錯誤");
+		}
+	});
 }
